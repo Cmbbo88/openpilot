8c47d8b256f8ab684e1a85201e09adf3edd7fbf9
Curtis Jenkins <curtis.x.jenkins@gmail.com>
2022-04-06 12:51:47 -0400

    rewrite buttonmodule code

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -246,6 +246,7 @@ class CarState(CarStateBase):
       ret.univACCenabled = False
       self.univACCtempdisable = False
       self.univACCspeed = 0
+      self.univbuttonState = pt_cp.vl["univACC"]['buttonState']
         # Only allow OP to enable if car is in D, S, manual, or tiptronic modes
       if pt_cp.vl["Getriebe_1"]['Waehlhebelposition__Getriebe_1_'] in [5, 9, 12, 14, 10, 11]:
         ret.cruiseState.available = True
@@ -253,40 +254,18 @@ class CarState(CarStateBase):
         ret.cruiseState.available = False
         ret.cruiseState.enabled = False
         ret.univACCenabled = False
-        # Toggle LKAS on, Toggle LKAS and ACC off
-      if pt_cp.vl["univACC"]['buttonState'] in [1]:
-        if not ret.cruiseState.enabled:
-          ret.cruiseState.enabled = True
-        else:
-          ret.cruiseState.enabled = False
-          ret.univACCenabled = False
-        # Toggle ACC on/off
-      if pt_cp.vl["univACC"]['buttonState'] in [2]:
-        if not ret.univACCenabled:
-          if self.univACCspeed == 0:
-            self.univACCspeed = 5 * round(ret.vEgo/5)
-          if ret.cruiseState.available:
-            ret.univACCenabled = True
-        else:
-          ret.univACCenabled = False
-        # Decrease set speed by 5
-      if pt_cp.vl["univACC"]['buttonState'] in [3]:
-        self.univACCspeed -= 5
-        if self.univACCspeed < 0:  # Not allow set speed to breach < 0 mph
-          self.univACCspeed = 0
-        # Increase set speed by 5
-      if pt_cp.vl["univACC"]['buttonState'] in [4]:
-        self.univACCspeed += 5
-        # Gas or Brake press = ACC disable, re-enable after brake release if under 15mph
+        # univButtonModule control
+      ret.cruiseState.enabled = False if (self.univbuttonState == 1 and ret.cruiseState.enabled) else True if (self.univbuttonState == 1 and not ret.cruiseState.enabled) else ret.cruiseState.enabled
+      ret.univACCenabled = False if (ret.cruiseState.enabled == False) else True if ((self.univbuttonState == 2 and not ret.univACCenabled) or (self.univACCtempdisable and not (ret.gasPressed or ret.brakePressed))) else False if (self.univbuttonState == 2 and ret.univACCenabled) else ret.univACCenabled
+      self.univACCspeed = 5 * round(ret.vEgo/5) if (self.univACCspeed == 0 and ret.univACCenabled) else + 5 if (self.univbuttonState == 4) else - 5 if (self.univbuttonState == 3) else self.univACCspeed
+      self.univACCtempdisable = True if ((ret.gasPressed or ret.brakePressed) and ret.vEgo < 15 * CV.MS_TO_MPH) else False if (ret.univACCenabled) else self.univACCtempdisable
+      if self.univACCspeed < 0:  # Not allow set speed to breach 0 mph
+        self.univACCspeed = 0
       if ret.gasPressed or ret.brakePressed:
         if ret.vEgo < 15 * CV.MS_TO_MPH:
           self.univACCtempdisable = True
         ret.univACCenabled = False
-      if self.univACCtempdisable and not (ret.gasPressed or ret.brakePressed):
-        ret.univACCenabled = True
-        self.univACCtempdisable = False
-        # Link univACCspeed to cruiseState.speed
-      ret.cruiseState.speed = self.univACCspeed * CV.KPH_TO_MS
+      ret.cruiseState.speed = self.univACCspeed * CV.KPH_TO_MS  # Link univACCspeed to cruiseState.speed
       # Stock OP function if univACC button module is not present
     else:
       ret.cruiseState.available = bool(pt_cp.vl["GRA_Neu"]['GRA_Hauptschalt'])
